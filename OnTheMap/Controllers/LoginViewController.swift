599fdfd75b992942730317d68b6463241d4865ad
Sri Harsha Chilakapati <hello@goharsha.com>
2021-03-21 16:45:38 +0530

    Design the login screen

---
+++ OnTheMap/Controllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Sri Harsha Chilakapati on 19/03/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onLoginButtonClicked() {
+    }
+    
+    @IBAction func onSignUpButtonClicked() {
+    }
+}
+
